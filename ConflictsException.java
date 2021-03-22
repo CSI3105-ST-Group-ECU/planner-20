9e32de92058848823b58d4e46577266799449040
Craig baldry <cbaldry@our.ecu.edu.au>
2021-03-22 15:42:06 +0800

    add files from zip

---
+++ ConflictsException.java
@@ -0,0 +1,27 @@
+/*modified code from University of South Carolina: package MeetingPlanner */
+package au.edu.sccs.csp3105.NBookingPlanner;
+
+
+public class ConflictsException extends Exception {
+	public ConflictsException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConflictsException(String message) {
+		super(message);
+		
+	}
+
+	public ConflictsException(Throwable cause) {
+		super(cause);
+	}
+
+	public ConflictsException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ConflictsException(String message, Throwable cause,boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		
+	}
+}
