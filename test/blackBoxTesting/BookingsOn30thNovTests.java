f6ff1cd3201a842a7c46b42f181c2ae975cdb645
Craig baldry <cbaldry@our.ecu.edu.au>
2021-04-22 16:11:19 +0800

    get rid of the system exit catch

--- test/blackBoxTesting/BookingsOn30thNovTests.java
+++ test/blackBoxTesting/BookingsOn30thNovTests.java
@@ -5,103 +5,150 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import au.edu.sccs.csp3105.NBookingPlanner.Planner;
 import blackBoxTesting.common.ConsoleOutput;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mockito;
 
 class BookingsOn30thNovTests {
+    Planner planner;
 
+    @BeforeEach
+    void setUp() {
+        planner = Mockito.spy(Planner.class);
+        Mockito.doNothing().when(planner).mainMenu();
+    }
+
+    @AfterEach
+    void tearDown() {
+        planner = null;
+    }
+
+    @Tag("NOV30")
+    @DisplayName("NOV30_1")
     @Test
-    void NOV30_SchedulingAMeetingOnThe30thNovTest() throws Exception {
-        String expected = ConsoleOutput.getValidScheduledMeetingOutput();
+    void NOV30_ValidSchedulingAMeetingOnThe30thNovTest() throws Exception {
+        String start_month = "11";
+        String start_day = "30";
+        String start_hour = "9";
+        String end_hour = "10";
+        String room_id = "JO18.330";
+        String name = "Mark Colin";
+        String done = "done";
+        String desc = "Happy Days";
 
-        String actual = tapSystemOutNormalized(() -> catchSystemExit(() -> withTextFromSystemIn(
-                "1",
-                "11",
-                "30",
-                "9",
-                "10",
-                "JO18.330",
-                "Mark Colin",
-                "done",
-                "Happy Days",
-                "0").execute(() -> Planner.main(null))));
+        String expected = ConsoleOutput.getValidScheduledMeetingOutput();
+        String actual = tapSystemOutNormalized(() -> withTextFromSystemIn(
+                start_month,
+                start_day,
+                start_hour,
+                end_hour,
+                room_id,
+                name,
+                done,
+                desc
+        ).execute(() -> planner.scheduleMeeting()));
 
         assertEquals(expected, actual);
     }
 
+    @Tag("NOV30")
+    @DisplayName("NOV30_3")
     @Test
     void NOV30_InvalidScheduleOnTheThirteenthMonthTest() throws Exception {
-        String notExpected = ConsoleOutput.getValidScheduledMeetingOutput();
+        String start_month = "13";
+        String start_day = "30";
+        String start_hour = "9";
+        String end_hour = "10";
+        String room_id = "JO18.330";
+        String name = "Mark Colin";
+        String done = "done";
+        String desc = "Happy Days";
 
-        String actual = tapSystemOutNormalized(() -> catchSystemExit(() -> withTextFromSystemIn(
-                "1",
-                "13",
-                "30",
-                "9",
-                "10",
-                "JO18.330",
-                "Mark Colin",
-                "done",
-                "Happy Days",
-                "0").execute(() -> Planner.main(null))));
+        String notExpected = ConsoleOutput.getValidScheduledMeetingOutput();
+        String actual = tapSystemOutNormalized(() -> withTextFromSystemIn(
+                start_month,
+                start_day,
+                start_hour,
+                end_hour,
+                room_id,
+                name,
+                done,
+                desc
+        ).execute(() -> planner.scheduleMeeting()));
 
         assertNotEquals(notExpected, actual);
     }
 
+
+    @Tag("NOV30")
+    @DisplayName("NOV30_4")
     @Test
     void NOV30_InvalidScheduleOnThe31DayTest() throws Exception {
-        String notExpected = ConsoleOutput.getValidScheduledMeetingOutput();
+        String start_month = "11";
+        String start_day = "31";
+        String start_hour = "9";
+        String end_hour = "10";
+        String room_id = "JO18.330";
+        String name = "Mark Colin";
+        String done = "done";
+        String desc = "Happy Days";
 
-        String actual = tapSystemOutNormalized(() -> catchSystemExit(() -> withTextFromSystemIn(
-                "1",
-                "11",
-                "31",
-                "9",
-                "10",
-                "JO18.330",
-                "Mark Colin",
-                "done",
-                "Happy Days",
-                "0").execute(() -> Planner.main(null))));
+        String notExpected = ConsoleOutput.getValidScheduledMeetingOutput();
+        String actual = tapSystemOutNormalized(() -> withTextFromSystemIn(
+                start_month,
+                start_day,
+                start_hour,
+                end_hour,
+                room_id,
+                name,
+                done,
+                desc
+        ).execute(() -> planner.scheduleMeeting()));
 
         assertNotEquals(notExpected, actual);
     }
 
+    @Tag("NOV30")
+    @DisplayName("NOV30_2")
     @Test
     void NOV30_ValidBookAVacationFrom29to30thNovTest() throws Exception {
-        String expected = ConsoleOutput.getValidVacationsOutput();
+        String start_month = "11";
+        String end_month = "11";
+        String start_day = "29";
+        String end_day = "30";
+        String name = "Mark Colin";
 
-        String actual = tapSystemOutNormalized(() -> catchSystemExit(() -> withTextFromSystemIn(
-                "2",
-                "11",
-                "11",
-                "29",
-                "30",
-                "Mark Colin",
-                "0").execute(() -> Planner.main(null))));
+        String expected = ConsoleOutput.getValidVacationsOutput();
+        String actual = tapSystemOutNormalized(() -> withTextFromSystemIn(
+                start_month,
+                end_month,
+                start_day,
+                end_day,
+                name
+        ).execute(() -> planner.scheduleVacation()));
 
         assertEquals(expected, actual);
     }
 
+    @Tag("NOV30")
+    @DisplayName("NOV30_5 - NOV30_8")
     @ParameterizedTest(name = "#{index}- Test with Argument={arguments}")
     @CsvSource({
-            "2,13,11,29,30,Mark Colin", //NOV30_4
-            "2,11,13,29,30,Mark Colin", //NOV30_5
-            "2,11,11,31,30,Mark Colin", //NOV30_6
-            "2,11,11,29,31,Mark Colin"  //NOV30_7
+            "13,11,29,30,Mark Colin", //NOV30_5
+            "11,13,29,30,Mark Colin", //NOV30_6
+            "11,11,31,30,Mark Colin", //NOV30_7
+            "11,11,29,31,Mark Colin"  //NOV30_8
     })
-    void NOV30_InvalidVacationsOnCertainDaysTest(String option, String start_month, String end_month, String start_day, String end_day, String name) throws Exception {
+    void NOV30_InvalidVacationsDaysOnNov30Test(String start_month, String end_month, String start_day, String end_day, String name) throws Exception {
         String expected = ConsoleOutput.getValidVacationsOutput();
-
-        String actual = tapSystemOutNormalized(() -> catchSystemExit(() -> withTextFromSystemIn(
-                option,
+        String actual = tapSystemOutNormalized(() -> withTextFromSystemIn(
                 start_month,
                 start_day,
                 end_month,
                 end_day,
-                name,
-                "0").execute(() -> Planner.main(null))));
+                name
+        ).execute(() -> planner.scheduleVacation()));
 
         assertNotEquals(expected, actual);
     }
