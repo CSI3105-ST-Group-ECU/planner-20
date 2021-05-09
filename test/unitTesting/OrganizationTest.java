e649cbe555fa53cd86db2ac2ceb6c29a0e4b6480
Craig baldry <cbaldry@our.ecu.edu.au>
2021-05-09 14:13:58 +0800

    setup test cases for unit testing organisation

--- test/unitTesting/OrganizationTest.java
+++ test/unitTesting/OrganizationTest.java
@@ -2,6 +2,7 @@ package unitTesting;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.fail;
@@ -20,9 +21,57 @@ class OrganizationTest {
 	void tearDown() throws Exception {
 	}
 
+	@DisplayName("UOG-1")
 	@Test
-	void test() {
+	void Test_initialisation_of_employee_List_and_person_list_with_correct_data() {
 		fail("Not yet implemented");
 	}
 
+	@DisplayName("UOG-2")
+	@Test
+	void Test_getters_for_employee_List_with_correct_data() {
+		fail("Not yet implemented");
+	}
+
+	@DisplayName("UOG-3")
+	@Test
+	void Test_getters_for_room_List_with_correct_data() {
+		fail("Not yet implemented");
+	}
+
+	@DisplayName("UOG-4")
+	@Test
+	void Test_returns_room_with_correct_id() {
+		fail("Not yet implemented");
+	}
+
+	@DisplayName("UOG-5")
+	@Test
+	void Test__throws_exception_room_does_not_exist() {
+		fail("Not yet implemented");
+	}
+
+	@DisplayName("UOG-6")
+	@Test
+	void Test_returns_room_on_second_iteration_with_correct_id() {
+		fail("Not yet implemented");
+	}
+
+	@DisplayName("UOG-7")
+	@Test
+	void Test_returns_employee_with_correct_name() {
+		fail("Not yet implemented");
+	}
+
+	@DisplayName("UOG-8")
+	@Test
+	void Test__throws_exception_employee_does_not_exist() {
+		fail("Not yet implemented");
+	}
+
+	@DisplayName("UOG-9")
+	@Test
+	void Test_returns_employee_on_second_iteration_with_correct_id() {
+		fail("Not yet implemented");
+	}
 }
