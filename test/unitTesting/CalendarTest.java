01d19ccc7028614a9758bc5437771bccf4c85612
Chris Raven <Chris@magentium.com>
2021-05-11 19:34:13 +0800

    Push for Testing

--- test/unitTesting/CalendarTest.java
+++ test/unitTesting/CalendarTest.java
@@ -8,12 +8,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 
-import java.util.Scanner;
-
-import static com.github.stefanbirkner.systemlambda.SystemLambda.*;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,7 +19,11 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class CalendarTest {
 
-	Calendar calendar;
+	Calendar calendar = new Calendar();
+	int month = 1;
+	int day = 1;
+	int start = 1;
+	int end = 2;
 	Exception cause;
 	String causeMessage;
 	String message;
@@ -33,37 +33,31 @@ class CalendarTest {
 	 */
 	@BeforeEach
 	void setUp() throws Exception {
-		Calendar calendar = new Calendar();
-		causeMessage = "Inception!";
-		message = "Conflict Exception Thrown";
-		cause = new Exception(causeMessage);
-	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterEach
-	void tearDown() throws Exception {
-	}
+		/**
+		 * @throws java.lang.Exception
+		 */
+		@AfterEach
+		void tearDown () throws Exception {
+		}
 
 
-	@DisplayName("CAL-1")
-	@Test
-	// Test creation of the Calendar Object
-	void Test_initialisation_of_no_arg_calendar() {
-		Calendar calendar = new Calendar();
-		assertTrue(calendar != null);
-	}
+		@DisplayName("CAL-1")
+		@Test
+		// Test creation of the Calendar Object
+		void Test_initialisation_of_no_arg_calendar () {
+			Calendar calendar = new Calendar();
+			assertTrue(calendar != null);
+		}
 
-	@DisplayName("CAL-2")
-	@Test
-	// Test if Busy returns False for a day not already booked
-	void Test_isBusy_Returns_False() throws ConflictsException {
-		Room room = new Room("Test");
-		Person person = new Person("Chris Raven");
+		@DisplayName("CAL-2")
+		@Test
+		// Test if Busy returns False for a day not already booked
+		void Test_isBusy_Returns_False () throws ConflictsException {
+			assertThrows(ConflictsException.class), () -> calendar.isBusy(1, 1, 2, 1), "FAILED";
+		}
 	}
 }
-
 
 
 
