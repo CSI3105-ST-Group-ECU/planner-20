9d6aa76bb63aac75afed5ce1223492172daa679d
JackCivil <Jackwcivil@outlook.com>
2021-05-11 10:38:07 +0800

    jack - unit testing

--- test/unitTesting/MeetingTest.java
+++ test/unitTesting/MeetingTest.java
@@ -1,19 +1,37 @@
 package unitTesting;
 
+import au.edu.sccs.csp3105.NBookingPlanner.Meeting;
+import au.edu.sccs.csp3105.NBookingPlanner.Person;
+import au.edu.sccs.csp3105.NBookingPlanner.Room;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
 /**
- * @author Jake
+ * @author Jack
  *
  */
-class MeetingTest {
 
-	@BeforeEach
-	void setUp() throws Exception {
+class MeetingTest {
+	int month; int day; int start; int end; String description;
+	Room room; ArrayList<Person> attendees;
+
+	public MeetingTest(){
+		this.month = 1;
+		this.day = 1;
+		this.start = 1;
+		this.end = 2;
+		this.description = "Test String";
+		this.room = new Room();
+		this.attendees = new ArrayList<>();
+		this.attendees.add(new Person());
+		this.attendees.add(new Person());
 	}
 
 	@AfterEach
@@ -22,7 +40,22 @@ class MeetingTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
-	}
 
+		//MEE-1
+		Meeting mee1 = new Meeting();
+		//MEE-2
+		Meeting mee2 = new Meeting(month, day);
+		//MEE-3
+		Meeting mee3 = new Meeting(month, day, description);
+		//MEE-4
+		Meeting mee4 = new Meeting(month, day, start, end);
+		//MEE-5
+		Meeting mee5 = new Meeting(month, day, start, end, attendees, room, description);
+
+		//MEE-6
+
+		//MEE-7
+
+		//MEE-8
+	}
 }
--- test/unitTesting/RoomTest.java
+++ test/unitTesting/RoomTest.java
@@ -2,14 +2,17 @@ package unitTesting;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
 /**
- * @author Everyone
+ * @author Jack
  *
  */
+
+@DisplayName("3. Room (ROO-*) - Unit Testing")
 class RoomTest {
 
 	@BeforeEach
@@ -21,8 +24,29 @@ class RoomTest {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test(){
+		//ROO-1
+
+		//ROO-2
+
+		//ROO-3
+
+		//ROO-4
+
+		//ROO-5
+
+		//ROO-6
+
+		//ROO-7
+
+		//ROO-8
+
+		//ROO-9
+
+		//ROO-10
+
+		//ROO-11
+
 	}
 
 }
